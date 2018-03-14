0f5fcb256578623685c981f60aaed293d0c93d10
Chris Steinle <steinle.4400@gmail.com>
2018-03-14 14:10:00 -0700

    added functionality to calculator

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
 $(document).ready(function() {
-
-
+	$('#add').on('click', function(){
+		var valOne = $('#numOne').val()
+		var valTwo = $('#numTwo').val()
+		console.log(`valOne = ${valOne}, valTwo = ${valTwo}`)
+		$('#answer').val(Number(valOne) + Number(valTwo))
+	})
+	$('#subtract').on('click', function(){
+		var valOne = $('#numOne').val()
+		var valTwo = $('#numTwo').val()
+		console.log(`valOne = ${valOne}, valTwo = ${valTwo}`)
+		$('#answer').val(Number(valOne) - Number(valTwo))
+	})
+	$('#multiply').on('click', function(){
+		var valOne = $('#numOne').val()
+		var valTwo = $('#numTwo').val()
+		console.log(`valOne = ${valOne}, valTwo = ${valTwo}`)
+		$('#answer').val(Number(valOne) * Number(valTwo))
+	})
+	$('#divide').on('click', function(){
+		var valOne = $('#numOne').val()
+		var valTwo = $('#numTwo').val()
+		console.log(`valOne = ${valOne}, valTwo = ${valTwo}`)
+		$('#answer').val(Number(valOne) / Number(valTwo))
+	})
 
 }) //Document Ready func
