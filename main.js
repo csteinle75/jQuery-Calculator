36e0931f83649d1abb9e9f695325d218324ca9df
Chris Steinle <steinle.4400@gmail.com>
2018-03-14 22:28:01 -0700

    all working as expected except decimal

--- main.js
+++ main.js
@@ -1,27 +1,178 @@
 $(document).ready(function() {
-	$('#add').on('click', function(){
+
+	//simple calc
+	$('#addSimple').on('click', function(){
 		var valOne = $('#numOne').val()
 		var valTwo = $('#numTwo').val()
-		console.log(`valOne = ${valOne}, valTwo = ${valTwo}`)
 		$('#answer').val(Number(valOne) + Number(valTwo))
 	})
-	$('#subtract').on('click', function(){
+	$('#subtractSimple').on('click', function(){
 		var valOne = $('#numOne').val()
 		var valTwo = $('#numTwo').val()
-		console.log(`valOne = ${valOne}, valTwo = ${valTwo}`)
 		$('#answer').val(Number(valOne) - Number(valTwo))
 	})
-	$('#multiply').on('click', function(){
+	$('#multiplySimple').on('click', function(){
 		var valOne = $('#numOne').val()
 		var valTwo = $('#numTwo').val()
-		console.log(`valOne = ${valOne}, valTwo = ${valTwo}`)
 		$('#answer').val(Number(valOne) * Number(valTwo))
 	})
-	$('#divide').on('click', function(){
+	$('#divideSimple').on('click', function(){
 		var valOne = $('#numOne').val()
 		var valTwo = $('#numTwo').val()
-		console.log(`valOne = ${valOne}, valTwo = ${valTwo}`)
 		$('#answer').val(Number(valOne) / Number(valTwo))
 	})
 
+	//advanced calc
+	let equation = []
+	let value = ''
+	let operator = ''
+	const result = $('#result')
+	const doTheMath = function(op){
+		switch (op){
+			case 'add':
+					console.log(`equation: ${equation}, value: ${value}, operator: ${operator}`)
+					equation = [equation.reduce((a,b)=> a + b)]	
+					result.val(equation[0])
+							
+				break;
+			case 'subtract':
+					console.log(`equation: ${equation}, value: ${value}, operator: ${operator}`)
+					equation = [equation.reduce((a,b)=> a - b)]
+					result.val(equation[0])
+					
+				break;
+			case 'multiply':
+					console.log(`equation: ${equation}, value: ${value}, operator: ${operator}`)
+					equation = [equation.reduce((a,b)=> a * b)]
+					result.val(equation[0])
+					
+				break;
+			case 'divide':
+					console.log(`equation: ${equation}, value: ${value}, operator: ${operator}`)
+					equation = [equation.reduce((a,b)=> a / b)]
+					result.val(equation[0])
+					
+				break;
+			default:
+					result.val(equation[0])
+					console.log(`equation: ${equation}, value: ${value}, operator: ${operator}`)
+				break;		
+		}
+	}
+
+	$('#clear').on('click', function(){
+		equation = []
+		value = ''
+		operator = ''
+		console.log(`equation: ${equation}, value: ${value}, operator: ${operator}`)
+		result.val('cleared')
+	})
+	$('#decimal').on('click', function(){
+		console.log(value.indexOf('.'))
+
+	})
+	
+	$('#seven').on('click', function(){
+		value += '7'
+		result.val(value)
+	})
+	$('#eight').on('click', function(){
+		value += '8'
+		result.val(value)
+	})
+	$('#nine').on('click', function(){
+		value += '9'
+		result.val(value)
+	})
+	$('#four').on('click', function(){
+		value += '4'
+		result.val(value)
+	})
+	$('#five').on('click', function(){
+		value += '5'
+		result.val(value)
+	})
+	$('#six').on('click', function(){
+		value += '6'
+		result.val(value)
+	})
+	$('#one').on('click', function(){
+		value += '1'
+		result.val(value)
+	})
+	$('#two').on('click', function(){
+		value += '2'
+		result.val(value)
+	})
+	$('#three').on('click', function(){
+		value += '3'
+		result.val(value)
+	})
+	$('#zero').on('click', function(){
+		value += '0'
+		result.val(value)
+	})
+
+
+
+
+	$('#add').on('click', function(){
+		if(value.length > 0){
+			equation.push(Number(value))
+		}
+		value = ''
+		if(equation.length === 2){
+			doTheMath(operator)
+		}
+		operator = 'add'
+		
+		console.log(`equation: ${equation}, value: ${value}, operator: ${operator}`)
+		result.val(equation[0])
+	})
+	$('#subtract').on('click', function(){
+		if(value.length > 0){
+			equation.push(Number(value))
+		}
+		value = ''
+		if(equation.length === 2){
+			doTheMath(operator)
+		}
+		operator = 'subtract'
+		
+		console.log(`equation: ${equation}, value: ${value}, operator: ${operator}`)
+		result.val(equation[0])
+	})
+	$('#multiply').on('click', function(){
+		if(value.length > 0){
+			equation.push(Number(value))
+		}
+		value = ''
+		if(equation.length === 2){
+			doTheMath(operator)
+		}
+		operator = 'multiply'
+		console.log(`equation: ${equation}, value: ${value}, operator: ${operator}`)
+		result.val(equation[0])
+	})
+	$('#divide').on('click', function(){
+		if(value.length > 0){
+			equation.push(Number(value))
+		}
+		value = ''
+		if(equation.length === 2){
+			doTheMath(operator)
+		}
+		operator = 'divide'
+		console.log(`equation: ${equation}, value: ${value}, operator: ${operator}`)
+		result.val(equation[0])
+	})
+
+	$('#equal').on('click', function(){
+		if(value.length > 0){
+			equation.push(Number(value))
+		}
+		value = ''
+		console.log('equal button pressed')
+		doTheMath(operator)
+	})
 }) //Document Ready func
